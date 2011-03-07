8e7d81ba2bab03cb2a95863364f3b4e4a18ec0c8
Paul deGrandis <paul.degrandis@gmail.com>
2011-03-06 22:37:15 -0800

    Fetch files and directories, raw or as json/stats. done.

--- src/clj_bitbucket/src.clj
+++ src/clj_bitbucket/src.clj
@@ -1,5 +1,22 @@
 (ns clj-bitbucket.src
   "BitBucket Source API features
   [http://confluence.atlassian.com/display/BBDEV/Source]"
-  (:require [clj-bitbucket.core :as core]))
+  (:require [clojure.string :as cstr]
+            [clj-bitbucket.core :as core]
+            [clj-bitbucket.repositories :as repos]))
+
+(defn fetch-source
+  ""
+  ([user repo rev path opts]
+   (fetch-source (cstr/join "/" user repo rev path) opts))
+  ([userrepo rev path opts]
+   (fetch-source (cstr/join "/" userrepo rev path) opts))
+  ([userrepopath rev opts]
+   ;; Split apart the userrepopath, insert after the repo the rev info, and rejoin the string
+   (fetch-source (cstr/join "/" (assoc (cstr/split userrepopath #"/" 2 (str "src/" rev)))) opts))
+  ([userreporevpath opts]
+   ;; We can get the raw file results if :raw is in opts
+   (if (opts :raw)
+     (repos/fetch-repo (.replaceFirst userreporevpath "/src/" "/raw/") opts)
+     (repos/fetch-repo userreporevpath opts))))
 
