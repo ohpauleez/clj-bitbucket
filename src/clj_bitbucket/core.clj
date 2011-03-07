ca6f3a8a51986111781aaa07fa0da389c2414326
Paul deGrandis <paul.degrandis@gmail.com>
2011-03-07 10:13:49 -0800

    Marg and Autodoc updates.  Core now has markdown formatted docs; examples in the docs to come

--- src/clj_bitbucket/core.clj
+++ src/clj_bitbucket/core.clj
@@ -1,31 +1,33 @@
-(ns clj-bitbucket.core
-  "The core functions for making requests to the BitBucket API;
+(ns 
+  ^{:doc "The core functions for making requests to the BitBucket API;  
   These are really only used by the other API files."
+    :author "[Paul deGrandis](https://github.com/ohpauleez)"}
+  clj-bitbucket.core
   (:require [clj-http.client :as client]
             [clj-json.core :as json]))
 
 
 (defn create-url
-  "Create a RESTful URL for a specifc version of the BitBucket API
-  Arguments:
-    url - a string, the tail end of the RESTful URL. ex: /respositories/some_user/some_repo/
-    [version] - a string, the API version you're accessing ['1.0']. ex: 1.0
-  Returns:
-     - a string, the final BitBucket API URL that should be called"
+  "Create a RESTful URL for a specifc version of the BitBucket API  
+  Arguments:  
+    url - a string, the tail end of the RESTful URL. ex: /respositories/some_user/some_repo/  
+    [version] - a string, the API version you're accessing ['1.0']. ex: 1.0  
+  Returns:  
+     - a string, the final BitBucket API URL that should be called"  
   ([url]
    (create-url url "1.0"))
   ([url version]
    (str "https://api.bitbucket.org/" version "/" url)))
 
 (defn create-request
-  "Create a request map for an API request, that can be serviced by the http client
-  Arguments:
-    method - a string or keyword, the HTTP method you're hitting the endpoint with. ex: :get
-    url - a string, the RESTful URL, as returned by create-url.
-    opts - a map, additional options for the BitBucket API or for the HTTP Client request
-    [ret-format] - a string, the format you want the RESTful call to return to you ['json']. ex: 'xml'
-  Returns:
-     - a map that can be passed directly to the http client to service."
+  "Create a request map for an API request, that can be serviced by the http client  
+  Arguments:  
+    method - a string or keyword, the HTTP method you're hitting the endpoint with. ex: :get  
+    url - a string, the RESTful URL, as returned by create-url.  
+    opts - a map, additional options for the BitBucket API or for the HTTP Client request  
+    [ret-format] - a string, the format you want the RESTful call to return to you ['json']. ex: 'xml'  
+  Returns:  
+     - a map that can be passed directly to the http client to service."  
   ([method url opts]
    (merge {:method (keyword method) :url (create-url url (get opts :api-version "1.0"))}
           (dissoc opts :api-version :auth :format)))
@@ -33,12 +35,12 @@
    (create-request method url (assoc-in opts [:query-params :format] ret-format))))
 
 (defn make-request
-  "Service a RESTful request.  See clj-http.client/request for more information.
-  Arguments:
-    req - a request map, as returned by create-request.
-    [auth] - a vector, [username password]
-  Returns:
-     - a string, the result of the servicing the call."
+  "Service a RESTful request.  See clj-http.client/request for more information.  
+  Arguments:  
+    req - a request map, as returned by create-request.  
+    [auth] - a vector, [username password]  
+  Returns:  
+     - a string, the result of the servicing the call."  
   ([req]
    (make-request {} req))
   ([req auth]
