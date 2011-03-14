3dbce1cd0194f438e25af9d99774203142d29b42
Paul deGrandis <paul.degrandis@gmail.com>
2011-03-13 18:30:55 -0700

    Follower and event support and docs for repositories

--- src/clj_bitbucket/repositories.clj
+++ src/clj_bitbucket/repositories.clj
@@ -1,23 +1,35 @@
-(ns clj-bitbucket.repositories
-  "BitBucket Repositories API features
-  [http://confluence.atlassian.com/display/BBDEV/Repositories]"
+(ns
+  ^{:doc "BitBucket Repositories API features
+  [http://confluence.atlassian.com/display/BBDEV/Repositories]
+
+  This also includes the events and followers RESTful calls associated with repositories"
+    :author "[Paul deGrandis](https://github.com/ohpauleez)"}
+  clj-bitbucket.repositories
   (:require [clj-bitbucket.core :as core]))
 
 
 (defn search-repos
+  "Search for repositories related to a query
+  Arguments:
+    query - a string, the subject matter you're trying to find repositories for
+    [opts] - a map, extra options to pass to core/create-request or core/create-request, such as :auth
+  Returns
+     - a map, with the list of repositories at :repositories.  Each repo will be a map."
   ([query]
    (search-repos query {}))
   ([query {:keys [auth ret-format] :or {auth {} ret-format "json"} :as opts}]
    (core/make-json-request
      (core/create-request :get "/repositories/"
                           (assoc-in opts [:query-params :name] query)
-                          ret-format))))
+                          ret-format)
+     auth)))
 
 (defn create-repo
   ""
   [])
 
 (defn fetch-repo
+  "Fetch a given repo, optionally passing in an options map for create-request/make-request"
   ([user repo opts]
    (search-repos (str user "/" repo) opts))
   ([userrepo]
@@ -31,6 +43,7 @@
      auth)))
 
 (defn tags
+  "Fetch the tags of a given repo"
   ([user repo opts]
    (tags (str user "/" repo) opts))
   ([userrepo]
@@ -39,6 +52,7 @@
    (fetch-repo (str userrepo "/tags/") opts)))
 
 (defn branches
+  "Fetch the branches of a given repo"
   ([user repo opts]
    (branches (str user "/" repo) opts))
   ([userrepo]
@@ -54,3 +68,25 @@
   ""
   [])
 
+
+;; Repository events
+;; =================
+(defn fetch-repo-events
+  "Fetch the events of a given repos"
+  ([user repo opts]
+   (fetch-repo-events (str user "/" repo) opts))
+  ([userrepo]
+   (fetch-repo-events userrepo {}))
+  ([userrepo opts]
+   (fetch-repo (str userrepo "/events") opts)))
+
+;; Repository followers
+;; ====================
+(defn fetch-repo-followers
+  "Fetch the followers of a given repos"
+  ([user repo opts]
+   (fetch-repo-events (str user "/" repo) opts))
+  ([userrepo]
+   (fetch-repo-events userrepo {}))
+  ([userrepo opts]
+   (fetch-repo (str userrepo "/followers") opts)))
